5ae6612fc2d5d986239027a549976ab1d41f5e49
newzpanuwat <new_sbh@me.com>
2018-07-08 21:01:03 +0700

    add more language with switch case

--- go-with-test/hello/hello.go
+++ go-with-test/hello/hello.go
@@ -7,16 +7,37 @@ import (
 )
 
 // Refractor by using constant
+const spanish = "Spanish"
+const french = "French"
+const englishPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-const helloPrefix = "Hello, "
-
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return helloPrefix + name
+	return greetingPrefix(language) + name
+
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Elodie", "Spanish"))
+	fmt.Println(Hello("Mbappe", "French"))
+	fmt.Println(Hello("Beckham", ""))
+	fmt.Println(Hello("Lampard", ""))
 }
