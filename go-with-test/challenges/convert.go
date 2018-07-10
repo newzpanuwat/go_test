1bf022c02ecc3f5fde7f79c3f5c13d07d852f381
newzpanuwat <new_sbh@me.com>
2018-07-11 00:00:53 +0700

    add convert func

---
+++ go-with-test/challenges/convert.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Convert(value float64) float64 {
+	return value * 37.5
+}
+
+func main() {
+	fmt.Println(Convert(2))
+}
--- go-with-test/challenges/converter_test.go
+++ go-with-test/challenges/converter_test.go
@@ -3,5 +3,10 @@ package challenges
 import "testing"
 
 func TestConvert(t *testing.T) {
+	got := Convert(1)
+	want := 37.5
 
+	if got != want {
+		t.Errorf("got '%f', want '%f'", got, want)
+	}
 }
